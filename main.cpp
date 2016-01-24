8b79726ace92a8ac8112d27f39b93eb5e3fe27f3
ReinhardProbst <reinhardprobst42@gmail.com>
2016-01-24 11:14:09 +0100

    Change module from class to struct

--- main.cpp
+++ main.cpp
@@ -2,46 +2,46 @@
 
 // Test concept of dataobject, reactor and modules
 
-class Module1
+struct Module1
 {
-    public:
-        // A module should have a constant name to identify it
-        const std::string _name;
+    // A module should have a constant name to identify it
+    const std::string _name;
 
-        // Only one constructor
-        Module1(std::string name) : _name(name), do1("DO1", 1), do2("DO2", "42") {}
+    // Only one constructor
+    Module1(std::string name) : _name(name), do1("DO1", 1), do2("DO2", "42") {}
 
-        Asm::DataObject<int> do1;
-        Asm::DataObject<std::string> do2;
+    Asm::DataObject<int> do1;
+    Asm::DataObject<std::string> do2;
 };
 
-class Module2
+struct Module2
 {
-    public:
-        // A module should have a constant name to identify it
-        const std::string _name;
+    // A module should have a constant name to identify it
+    const std::string _name;
 
-        // Only one constructor
-        Module2(std::string name) : _name(name), do1("DO1", 2), do2("DO2", std::map<std::string, double>{{"42", 22.0}, {"43", 23.0}, {"44", 24.0}}) {}
+    // Only one constructor
+    Module2(std::string name) : _name(name), do1("DO1", 2), do2("DO2", std::map<std::string, double>{{"42", 22.0}, {"43", 23.0}, {"44", 24.0}}) {}
 
-        Asm::DataObject<int> do1;
-        Asm::DataObject<std::map<std::string, double>> do2;
+    Asm::DataObject<int> do1;
+    Asm::DataObject<std::map<std::string, double>> do2;
 
-        void link1(Asm::DataObject<int> &do1, Asm::DataObject<int> &do2)
-        {
-            std::cout << "My DO.name: " << do2.getName() << std::endl;
-            std::cout << "Got DO.name: " << do1.getName() << std::endl;
-            do1.get([](int i){ std::cout << "Got DO.value: " << i << std::endl; });
-            do2.get([](int i){ std::cout << "Had DO.value: " << i << std::endl; });
-        }
+    void link1(Asm::DataObject<int> &do1, Asm::DataObject<int> &do2)
+    {
+        std::cout << "My Module.name: " << _name << std::endl;
+        std::cout << "My DO.name: " << do2.getName() << std::endl;
+        std::cout << "Got DO.name: " << do1.getName() << std::endl;
+        do1.get([](int i){ std::cout << "Got DO.value: " << i << std::endl; });
+        do2.get([](int i){ std::cout << "Had DO.value: " << i << std::endl; });
+    }
 
-        void link2(Asm::DataObject<std::string> &do1, Asm::DataObject<std::map<std::string, double>> &do2)
-        {
-            std::cout << "My DO.name: " << do2.getName() << std::endl;
-            std::cout << "Got DO.name: " << do1.getName() << std::endl;
-            std::string s = do1.get([](std::string s){ std::cout << "Got DO.value: " << s << std::endl; return s; });
-            std::cout << "Has DO.value: " << do2.get([&s](const std::map<std::string, double> &m){ return m.at(s); }) << '\n';
-        }
+    void link2(Asm::DataObject<std::string> &do1, Asm::DataObject<std::map<std::string, double>> &do2)
+    {
+        std::cout << "My Module.name: " << _name << std::endl;
+        std::cout << "My DO.name: " << do2.getName() << std::endl;
+        std::cout << "Got DO.name: " << do1.getName() << std::endl;
+        std::string s = do1.get([](std::string s){ std::cout << "Got DO.value: " << s << std::endl; return s; });
+        std::cout << "Has DO.value: " << do2.get([&s](const std::map<std::string, double> &m){ return m.at(s); }) << '\n';
+    }
 };
 
 // Helper function template
