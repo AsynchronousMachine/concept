6b794a01e614f6bacf4925392bb2bbe2806f7c34
Egmond Krebs <e.krebs@erhardt-leimer.com>
2017-06-01 13:48:19 +0200

    make TimerObject instantiated under Windows without a function

--- src/asm/timerobject.hpp
+++ src/asm/timerobject.hpp
@@ -9,7 +9,7 @@
 
 namespace Asm {
 
-#ifdef __linux__
+
 	class TimerObject
 	{
 		friend class TimerObjectReactor; // This enables the timer reactor to access the internals
@@ -22,10 +22,12 @@ namespace Asm {
 	public:
 		TimerObject() : _fd(-1), _interval(0), _next(0)
 		{
+#ifdef __linux__
 			if ((_fd = ::timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC)) < 0)
 			{
 				std::cout << "Timer could not be created: " << std::strerror(errno) << std::endl;
 			}
+#endif
 		}
 
 		// Non-copyable
@@ -38,12 +40,15 @@ namespace Asm {
 
 		~TimerObject()
 		{
+#ifdef __linux__
 			stop();
 			::close(_fd);
+#endif
 		}
 
 		bool setRelativeInterval(long interval, long next = 0)
 		{
+#ifdef __linux__
 			itimerspec val;
 
 			if (interval == 0 && next == 0)
@@ -66,12 +71,13 @@ namespace Asm {
 				std::cout << "Timer could not set: " << std::strerror(errno) << std::endl;
 				return false;
 			}
-
+#endif
 			return true;
 		}
 
 		bool stop()
 		{
+#ifdef __linux__
 			itimerspec val{};
 
 			if (::timerfd_settime(_fd, 0, &val, 0) < 0)
@@ -79,7 +85,7 @@ namespace Asm {
 				std::cout << "Timer could not stopped: " << std::strerror(errno) << std::endl;
 				return false;
 			}
-
+#endif
 			return true;
 		}
 
@@ -89,14 +95,16 @@ namespace Asm {
 
 		bool wait(uint64_t& elapsed)
 		{
+#ifdef __linux__
 			if (::read(_fd, &elapsed, sizeof(uint64_t)) != sizeof(uint64_t))
 			{
 				std::cout << "Timer could not read: " << std::strerror(errno) << std::endl;
 				return false;
 			}
-
+#endif
 			return true;
 		}
+
 	};
-#endif
+
 }
