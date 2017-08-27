25568b388e87cd68f732f1a2e2e8d9211bfb8aa3
ReinhardProbst <reinhardprobst42@gmail.com>
2017-08-27 17:58:14 +0200

    All modules printing in systemmodule added

--- src/maker/maker_reflection.cpp
+++ src/maker/maker_reflection.cpp
@@ -3,16 +3,17 @@
 
 // Get out the module name for humans
 const id_string_map module_name {
-    {&sysModule, "SystemModule.sysModule"},
 	{&inModule, "InputModule.inModule"},
 	{&outModule, "OutputModule.outModule"},
 	{&processModule, "ProcessModule.processModule"},
 	{&serModule, "SerializeModule.serModule"},
+	{&sysModule, "SystemModule.sysModule"},
 	{&tbbModule, "TBBModule.tbbModule"},
 };
 
 // Get out the DO name for humans
-const id_string_map do_names {
+const id_string_map do_names
+{
 	{&inModule.DOintOutput, "InputModule.inModule.DOintOutput"},
 	{&inModule.DOstringOutput, "InputModule.inModule.DOstringOutput"},
 	{&outModule.DOintInput, "OutputModule.outModule.DOintInput"},
@@ -25,6 +26,9 @@ const id_string_map do_names {
 	{&serModule.doString2, "SerializeModule.serModule.doString2"},
 	{&serModule.doString3, "SerializeModule.serModule.doString3"},
 	{&serModule.doMyComplexDOType, "SerializeModule.serModule.doMyComplexDOType"},
+	{&sysModule.serializeAll, "SystemModule.sysModule.serializeAll"},
+	{&sysModule.deserializeAll, "SystemModule.sysModule.deserializeAll"},
+	{&sysModule.printModules, "SystemModule.sysModule.printModules"},
 	{&tbbModule.DOcomplexInOut, "TBBModule.tbbModule.DOcomplexInOut"},
 };
 
@@ -34,12 +38,11 @@ const print_module_map print_modules {
 	{"OutputModule.outModule","  |> DataObject<int> DOintInput\n  |> DataObject<std::string> DOstringInput\n  |> LinkObject<DataObject<MyComplexDOType>, DataObject<int> > LinkInt\n  |> LinkObject<DataObject<MyComplexDOType>, DataObject<std::string> > LinkString\n"},
 	{"ProcessModule.processModule","  |> DataObject<MyComplexDOType> DOcomplexInOut\n  |> LinkObject<DataObject<int>, DataObject<MyComplexDOType> > LinkInt\n  |> LinkObject<DataObject<std::string>, DataObject<MyComplexDOType> > LinkString\n"},
 	{"SerializeModule.serModule","  |> DataObject<int> doInt\n  |> DataObject<double> doDouble\n  |> DataObject<std::string> doString\n  |> DataObject<bool> doBool\n  |> DataObject<std::string> doString2\n  |> DataObject<std::string> doString3\n  |> DataObject<MyComplexDOType> doMyComplexDOType\n"},
+	{"SystemModule.sysModule","  |> DataObject<bool> serializeAll\n  |> DataObject<bool> deserializeAll\n  |> DataObject<bool> printModules\n"},
 	{"TBBModule.tbbModule","  |> DataObject<MyComplexDOType> DOcomplexInOut\n  |> LinkObject<DataObject<int>, DataObject<MyComplexDOType> > LinkInt\n"},
 };
 
 const name_dataobject_map name_dataobjects {
-    {"SystemModule.sysModule.deserializeAll", sysModule.deserializeAll},
-	{"SystemModule.sysModule.serializeAll", sysModule.serializeAll},
 	{"InputModule.inModule.DOintOutput", inModule.DOintOutput},
 	{"InputModule.inModule.DOstringOutput", inModule.DOstringOutput},
 	{"OutputModule.outModule.DOintInput", outModule.DOintInput},
@@ -52,6 +55,9 @@ const name_dataobject_map name_dataobjects {
 	{"SerializeModule.serModule.doString2", serModule.doString2},
 	{"SerializeModule.serModule.doString3", serModule.doString3},
 	{"SerializeModule.serModule.doMyComplexDOType", serModule.doMyComplexDOType},
+	{"SystemModule.sysModule.serializeAll", sysModule.serializeAll},
+	{"SystemModule.sysModule.deserializeAll", sysModule.deserializeAll},
+	{"SystemModule.sysModule.printModules", sysModule.printModules},
 	{"TBBModule.tbbModule.DOcomplexInOut", tbbModule.DOcomplexInOut},
 };
 
--- src/modules/SystemModule.hpp
+++ src/modules/SystemModule.hpp
@@ -13,87 +13,110 @@
 #include "../../external/rapidjson/include/rapidjson/error/en.h"
 
 class SystemModule {
-private:
+  private:
 
-	void emptySer(rapidjson::Value& value, rapidjson::Document::AllocatorType& allocator) {
-        std::cout << "Serialize not implemented" << std::endl; }
+    void emptySer(rapidjson::Value& value, rapidjson::Document::AllocatorType& allocator) {
+        std::cout << "Serialize not implemented" << std::endl;
+    }
 
-	void runSerializeAll(rapidjson::Value& value) {
-		if (value.IsBool() && value.GetBool()) {
-			std::cout << "Dump <system-dump.json> to disk" << std::endl;
-			using namespace rapidjson;
-			Document doc; // Null
-			doc.SetObject();
+    void emptyDeser(rapidjson::Value& value) {
+        std::cout << "Deserialize not implemented" << std::endl;
+    }
 
-			std::string key;
-			Value json_key(kStringType);
-			Value json_value;
+    void runSerializeAll(rapidjson::Value& value) {
+        if (value.IsBool() && value.GetBool()) {
+            std::cout << "Dump <system-dump.json> to disk" << std::endl;
+            using namespace rapidjson;
+            Document doc; // Null
+            doc.SetObject();
 
-			//iterate over all public DOs
-			for (auto map_iter : name_dataobjects)
-			{
-				boost::apply_visitor([&](auto& d) { d.serialize(json_value, doc.GetAllocator()); }, map_iter.second);
-				if (!json_value.IsNull()) {
-					key = map_iter.first;
-					//json_key.SetString(StringRef(*key.c_str)); // always the last key!!!
-					json_key.SetString(key.c_str(), doc.GetAllocator());
-					doc.AddMember(json_key, json_value, doc.GetAllocator());
-				}
-			}
+            std::string key;
+            Value json_key(kStringType);
+            Value json_value;
 
-			std::FILE* fp = std::fopen("system-dump.json", "wb"); // non-Windows use "w"
-			char writeBuffer[1000000];
-			FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
-			PrettyWriter<FileWriteStream> writer(os);
-			doc.Accept(writer);
-			fclose(fp);
-		}
-	}
+            //iterate over all public DOs
+            for (auto map_iter : name_dataobjects) {
+                boost::apply_visitor([&](auto& d) {
+                    d.serialize(json_value, doc.GetAllocator());
+                }, map_iter.second);
+                if (!json_value.IsNull()) {
+                    key = map_iter.first;
+                    //json_key.SetString(StringRef(*key.c_str)); // always the last key!!!
+                    json_key.SetString(key.c_str(), doc.GetAllocator());
+                    doc.AddMember(json_key, json_value, doc.GetAllocator());
+                }
+            }
 
-	void runDeserializeAll(rapidjson::Value& value) {
-		if (value.IsBool() && value.GetBool()) {
+            std::FILE* fp = std::fopen("system-dump.json", "wb"); // non-Windows use "w"
+            char writeBuffer[1000000];
+            FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
+            PrettyWriter<FileWriteStream> writer(os);
+            doc.Accept(writer);
+            fclose(fp);
+        }
+    }
+
+    void runDeserializeAll(rapidjson::Value& value) {
+        if (value.IsBool() && value.GetBool()) {
             std::cout << "Try to read <system-dump.json> from disk" << std::endl;
 
-			if (!std::experimental::filesystem::exists("system-dump.json"))
-				return;
+            if (!std::experimental::filesystem::exists("system-dump.json"))
+                return;
 
-			using namespace rapidjson;
-			std::FILE* fp = std::fopen("system-dump.json", "rb"); // non-Windows use "r"
-			char readBuffer[65536];
-			FileReadStream is(fp, readBuffer, sizeof(readBuffer));
-			Document doc;
-			doc.ParseStream(is);
-			fclose(fp);
+            using namespace rapidjson;
+            std::FILE* fp = std::fopen("system-dump.json", "rb"); // non-Windows use "r"
+            char readBuffer[65536];
+            FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+            Document doc;
+            doc.ParseStream(is);
+            fclose(fp);
 
-			if (doc.HasParseError() == true) {
+            if (doc.HasParseError() == true) {
                 std::cout << "Deserialize has parsing error at offset " << doc.GetErrorOffset() << std::endl;
                 std::cout << ">>>" << GetParseError_En(doc.GetParseError()) << std::endl;
-				return;
-			}
+                return;
+            }
 
-			//iterate over all DO-keys from json file
-			for (Value::ConstMemberIterator itr = doc.MemberBegin(); itr != doc.MemberEnd(); ++itr)
-			{
-				//const char* doName = itr->name.GetString();
-				Value& v = doc[itr->name.GetString()];
-				try {
-					boost::apply_visitor([&](auto& d) { d.deserialize(v); }, name_dataobjects.at(itr->name.GetString()));
-				}
-				catch (const std::exception& e) {
-				    std::cerr << "Deserialize exeption: " << e.what() << std::endl;
-					std::cout << "Missing " << itr->name.GetString() << std::endl;
-				}
+            //iterate over all DO-keys from json file
+            for (Value::ConstMemberIterator itr = doc.MemberBegin(); itr != doc.MemberEnd(); ++itr) {
+                //const char* doName = itr->name.GetString();
+                Value& v = doc[itr->name.GetString()];
+                try {
+                    boost::apply_visitor([&](auto& d) {
+                        d.deserialize(v);
+                    }, name_dataobjects.at(itr->name.GetString()));
+                } catch (const std::exception& e) {
+                    std::cerr << "Deserialize exeption: " << e.what() << std::endl;
+                    std::cout << "Missing " << itr->name.GetString() << std::endl;
+                }
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-public:
-	//have to be public, else they are not in the maps build by the map maker tool
-	Asm::DataObject<bool> serializeAll;
-	Asm::DataObject<bool> deserializeAll;
+    void runPrintModules(rapidjson::Value& value, rapidjson::Document::AllocatorType& allocator) {
+        rapidjson::Value rjvalue;
 
-	SystemModule() :
-		serializeAll(false, &SystemModule::emptySer, &SystemModule::runSerializeAll, this),
-		deserializeAll(false, &SystemModule::emptySer, &SystemModule::runDeserializeAll, this) {}
+        value.SetObject();
+
+        std::cout << "This is for humans:" << std::endl;
+
+        for(const auto& m : print_modules) {
+            rjvalue.SetString(m.second.c_str(), allocator);
+            value.AddMember(rapidjson::StringRef(m.first.c_str()), rjvalue, allocator);
+
+            std::cout << m.first << std::endl << m.second << std::endl;
+        }
+    }
+
+  public:
+    // This have to be public, else they are not in the maps build by the map maker tool
+    Asm::DataObject<bool> serializeAll;
+    Asm::DataObject<bool> deserializeAll;
+    Asm::DataObject<bool> printModules;
+
+    SystemModule() :
+        serializeAll(false, &SystemModule::emptySer, &SystemModule::runSerializeAll, this),
+        deserializeAll(false, &SystemModule::emptySer, &SystemModule::runDeserializeAll, this),
+        printModules(false, &SystemModule::runPrintModules, &SystemModule::emptyDeser, this) {}
 };
