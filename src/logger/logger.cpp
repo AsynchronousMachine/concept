da3d1a8f64fa2a78cf1b3d124d60ad6aeb82b799
ReinhardProbst <reinhardprobst42@gmail.com>
2017-09-10 11:31:00 +0200

    Error handling for constructor added

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -8,6 +8,6 @@
 namespace Logger {
 
 // Logger should be ready before any other classes or variables
-std::unique_ptr<Logger> __attribute__((init_priority(1000))) pLOG  = std::make_unique<Logger>("Asm");
+std::unique_ptr<Logger> __attribute__((init_priority(1000))) pLOG = std::make_unique<Logger>("Asm");
 
 }
--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -5,9 +5,12 @@
 
 #pragma once
 
-#include <memory>
+#include <syslog.h>
 
-// Adjust settings for spdlog, ref. spdlog/tweakme.h
+#include <memory>
+#include <exception>
+
+// Adjust settings for spdlog, refer to, spdlog/tweakme.h
 #define SPDLOG_FINAL final
 #define SPDLOG_PREVENT_CHILD_FD
 #define SPDLOG_ENABLE_SYSLOG
@@ -16,33 +19,45 @@
 namespace Logger {
 
 class Logger {
-    std::shared_ptr<spdlog::logger> _console = nullptr;
-    std::shared_ptr<spdlog::logger> _syslog = nullptr;
+    std::shared_ptr<spdlog::logger> _console;
+    std::shared_ptr<spdlog::logger> _syslog;
 
   public:
     Logger(const std::string& name) {
+        try
+        {
 #ifdef SPDLOG_TRACE_ON
-
-        // Create console logger
-        _console = spdlog::stdout_color_mt(name + ":stdout");
-        _console->set_level(spdlog::level::trace);
-        _console->set_pattern("[%b %d %H:%M:%S][%n] %v");
-        _console->trace(">>>Tracing on stdout is enabled<<<");
-
+            // Create console logger
+            _console = spdlog::stdout_color_mt(name);
+            _console->set_pattern("[%Y-%m-%d %T][%n] %v");
+            _console->set_level(spdlog::level::trace);
+            _console->trace("+++Tracing on stdout is enabled+++");
 #endif
 
 #ifdef __linux__
-
-        // Create Linux syslog logger
-        _syslog = spdlog::syslog_logger("syslog", name);
-        info(">>>Syslog is enabled<<<");
-#ifdef SPDLOG_DEBUG_ON
-        _syslog->set_level(spdlog::level::debug);
-#endif
-
+            // Create Linux syslog logger
+            _syslog = spdlog::syslog_logger("syslog", name);
 #else // Windows
-
+            // Create Windows syslog logger
+            // _syslog = spdlog::rotating_logger_mt(name, "syslog", 1024 * 1024 * 5, 10);
+            // _syslog->set_pattern("[%Y-%m-%d %T][%n] %v");
 #endif
+
+            info("+++Syslog is enabled+++");
+#ifdef SPDLOG_DEBUG_ON
+            _syslog->set_level(spdlog::level::debug);
+#endif
+        }
+        catch (const std::exception& ex)
+        {
+            // Due to the early stage initialization, refer __attribute__((init_priority(1000))), std::cout is not valid yet
+            ::openlog(name.data(), LOG_PERROR | LOG_NDELAY, LOG_USER);
+            ::syslog(LOG_ALERT, "---Initialization of logger failed: %s---", ex.what());
+            ::closelog();
+
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+            throw; // Rethrows the exception object of type std::exception, without logger nothing makes sense
+        }
     }
 
     // Necessary if someone want to inherit from that
